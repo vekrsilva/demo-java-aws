31428c3a2bc4fb727717533394da0580e86ee513
vekrsilva <vekrsilva@gmail.com>
2026-04-15 16:05:20 -0300

    init

---
+++ src/main/java/br/com/aws_demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.aws_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Bem-vindo ao MeuSite!";
+    }
+}
